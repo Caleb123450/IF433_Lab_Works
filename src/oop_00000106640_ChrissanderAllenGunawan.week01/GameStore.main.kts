a1e809c6897426f77bf90b044a468112ce6644d8
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-02-09 20:26:05 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000106640_ChrissanderAllenGunawan.week01/GameStore.main.kts
+++ src/oop_00000106640_ChrissanderAllenGunawan.week01/GameStore.main.kts
@@ -3,5 +3,9 @@ package oop_00000106640_ChrissanderAllenGunawan.week01
 fun main() {
     val gameTitle = "FIFA 25"
     val price = 120000
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
 }
+
+fun calculateDiscount(price: Int) = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
 
