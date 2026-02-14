0818f3f99d9b8b4578b9e579297402f49c16de91
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-02-14 13:41:18 +0700

    week02: add secondary constructor

--- src/oop_00000106640_ChrissanderAllenGunawan.week02/Student.kt
+++ src/oop_00000106640_ChrissanderAllenGunawan.week02/Student.kt
@@ -6,12 +6,15 @@ class Student (
     var major: String
 ) {
     init {
-        // Validasi Cek panjang dari NIM
         if (nim.length != 5) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
             println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
         } else {
             println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
         }
+    }
+
+    constructor(name: String, nim: String) : this(name, nim, major = "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
     }
 }
